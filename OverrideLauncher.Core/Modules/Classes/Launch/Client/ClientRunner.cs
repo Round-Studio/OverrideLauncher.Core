46d96c8283d61b6c728a510d9724b5cadf22cb6a
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-05-25 00:59:03 +0800

    🌐 删除测试代码

--- OverrideLauncher.Core/Modules/Classes/Launch/Client/ClientRunner.cs
+++ OverrideLauncher.Core/Modules/Classes/Launch/Client/ClientRunner.cs
@@ -25,7 +25,6 @@ public class ClientRunner
                 RedirectStandardInput = true
             }
         };
-        File.WriteAllText("a.bat",$"\"{RunnerInfo.JavaInfo.JavaPath}\" {arg}");
         GameProcess.OutputDataReceived += (sender, args) =>
         {
             LogsOutput.Invoke(args.Data);
