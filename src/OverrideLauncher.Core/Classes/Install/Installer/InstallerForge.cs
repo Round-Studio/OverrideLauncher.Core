9b6d342ee4943c2ea42ffe5145350b6adf34ada5
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-07-23 13:38:31 +0800

    feat: Forge 安装外壳（无）

---
+++ src/OverrideLauncher.Core/Classes/Install/Installer/InstallerForge.cs
@@ -0,0 +1,17 @@
+﻿using OverrideLauncher.Core.Base.Entry.Download.Install.Client;
+using OverrideLauncher.Core.Interface.Download;
+
+namespace OverrideLauncher.Core.Classes.Install.Installer;
+
+public class InstallerForge : IDownload
+{
+    public InstallerForge()
+    {
+        
+    }
+
+    public async Task Install(ClientRootInfo rootInfo)
+    {
+        
+    }
+}
